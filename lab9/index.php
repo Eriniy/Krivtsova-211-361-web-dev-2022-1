1c6ddd533071eefc6ee669ba3503c03ef8bba5b9
Julia Krivtsova <90450139+Eriniy@users.noreply.github.com>
2022-12-05 23:56:54 +0300

    lab 9

---
+++ lab9/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8" />
+        <title>Кривцова Ю.С. 211-361 №9</title>
+        <!-- подключение стилей gooleFonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Andika:wght@400;700&display=swap" rel="stylesheet">
+        <!--Подключение файла со стилеями-->
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+        <div class="wrapper">
+            <header class="header">
+                <div class="header">
+                    <div class="inline"><img src="polytech_logo_main_RGB.png"></div>
+                    <div class="inline title">Кривцова Ю.С. 211-361 №9</div>
+                </div>
+                
+    
+            </header>
+            <main>
+                <?php
+                    include "funcWork.php";
+                ?> 
+
+            </main>
+            <footer>
+                <div class="footer">
+                    <span class="left">
+                        <span>Все права защищены &copy; by krivtsova</span><br>
+                        <span>Тип верстки: <?=$type?></span>
+                    </span>
+                    <span class="right">
+                        <span>+7 (991) 648-48-27</span><br>
+                        <span>yul.krivaya@bk.ru</span>
+                    </span>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
